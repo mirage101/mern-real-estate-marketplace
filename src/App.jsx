f2bc1879740be385009770b6f61916e3027e593f
Ivan Filipov <ivan@stanga.net>
2024-02-13 16:16:11 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignUp";
 
 export default function App() {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          Home
+        </Route>
+        <Route path="/sign-in" element={<SignIn />}>
+          Sign In
+        </Route>
+        <Route path="/sign-up" element={<SignUp />}>
+          Sign Up
+        </Route>
+        <Route path="/about" element={<About />}>
+          About
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+          Profile
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
